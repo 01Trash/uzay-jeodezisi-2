5787f16263b49efd00128135efaba3b1de1a8da8
Erdem <gnserdem13@gmail.com>
2022-05-17 15:01:59 +0300

    adsf

---
+++ files/Run_SP3_read.bat
@@ -0,0 +1,113 @@
+ECHO ON
+
+
+PRNfromSP3  G22  2022  04  15
+rename G22.xyz  G22.txt
+
+PRNfromSP3  G13  2022  04  15
+rename G13.xyz  G13.txt
+
+PRNfromSP3  G28  2022  04  15
+rename G28.xyz  G28.txt
+
+PRNfromSP3  G21  2022  04  15
+rename G21.xyz  G21.txt
+
+PRNfromSP3  G07  2022  04  15
+rename G07.xyz  G07.txt
+
+PRNfromSP3  G05  2022  04  15
+rename G05.xyz  G05.txt
+
+PRNfromSP3  G20  2022  04  15
+rename G20.xyz  G20.txt
+
+PRNfromSP3  G17  2022  04  15
+rename G17.xyz  G17.txt
+
+PRNfromSP3  G15  2022  04  15
+rename G15.xyz  G15.txt
+
+PRNfromSP3  G16  2022  04  15
+rename G16.xyz  G16.txt
+
+PRNfromSP3  G12  2022  04  15
+rename G12.xyz  G12.txt
+
+PRNfromSP3  G19  2022  04  15
+rename G19.xyz  G19.txt
+
+PRNfromSP3  G02  2022  04  15
+rename G02.xyz  G02.txt
+
+PRNfromSP3  G25  2022  04  15
+rename G25.xyz  G25.txt
+
+PRNfromSP3  G01  2022  04  15
+rename G01.xyz  G01.txt
+
+PRNfromSP3  G30  2022  04  15
+rename G30.xyz  G30.txt
+
+PRNfromSP3  G27  2022  04  15
+rename G27.xyz  G27.txt
+
+PRNfromSP3  R09  2022  04  15
+rename R09.xyz  R09.txt
+
+PRNfromSP3  R11  2022  04  15
+rename R11.xyz  R11.txt
+
+PRNfromSP3  R20  2022  04  15
+rename R20.xyz  R20.txt
+
+PRNfromSP3  R19  2022  04  15
+rename R19.xyz  R19.txt
+
+PRNfromSP3  R13  2022  04  15
+rename R13.xyz  R13.txt
+
+PRNfromSP3  R01  2022  04  15
+rename R01.xyz  R01.txt
+
+PRNfromSP3  R22  2022  04  15
+rename R22.xyz  R22.txt
+
+PRNfromSP3  R08  2022  04  15
+rename R08.xyz  R08.txt
+
+PRNfromSP3  R03  2022  04  15
+rename R03.xyz  R03.txt
+
+PRNfromSP3  R07  2022  04  15
+rename R07.xyz  R07.txt
+
+PRNfromSP3  R02  2022  04  15
+rename R02.xyz  R02.txt
+
+PRNfromSP3  R17  2022  04  15
+rename R17.xyz  R17.txt
+
+PRNfromSP3  R14  2022  04  15
+rename R14.xyz  R14.txt
+
+PRNfromSP3  R18  2022  04  15
+rename R18.xyz  R18.txt
+
+PRNfromSP3  R21  2022  04  15
+rename R21.xyz  R21.txt
+
+PRNfromSP3  R05  2022  04  15
+rename R05.xyz  R05.txt
+
+PRNfromSP3  R15  2022  04  15
+rename R15.xyz  R15.txt
+
+PRNfromSP3  R12  2022  04  15
+rename R12.xyz  R12.txt
+
+PRNfromSP3  R24  2022  04  15
+rename R24.xyz  R24.txt
+
+PAUSE
+
